4401f16070d37c3fe4a48cdda987a147e948ae59
zach <zachariah.eb@protonmail.com>
2024-02-05 23:42:09 -0600

    linux build on latest fork update

--- ui/src/custom_components/index.ts
+++ ui/src/custom_components/index.ts
@@ -3,10 +3,25 @@
 
 import BubbleMessage from './BubbleMessage.vue';
 import BubbleMessageAdvanced from './BubbleMessageAdvanced.vue';
+import CustomSection from './CustomCoreSection.vue';
+import CustomButton from './CustomCoreButton.vue';    
+import CustomDropDown from './input/CoreDropdownInput.vue';
+import CustomTextInput from './input/CoreTextInput.vue';
+import CustomNumberInput from './input/CustomNumberInput.vue';
+import CustomTabs from './CustomTabs.vue';
+import CustomTab from './CustomTab.vue';
+
 
 // Export an object with the ids and the templates as default
 
 export default {
     "bubblemessage": BubbleMessage,
-    "bubblemessageadvanced": BubbleMessageAdvanced
+    "bubblemessageadvanced": BubbleMessageAdvanced,
+    "customsection": CustomSection,
+    "custombutton": CustomButton,
+    "customdropdown": CustomDropDown,
+    "customtextinput": CustomTextInput,
+    "customnumberinput": CustomNumberInput,
+    "customtabs": CustomTabs,
+    "customtab": CustomTab,
 }
